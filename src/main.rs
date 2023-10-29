52836a51aee9e0a991e0a40c1b361cdbc7842e21
Matteo Tedesco <matteotedesco@outlook.com>
2023-10-29 23:28:19 +0100

    added functions to take screenshots and save files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod utility;
+use utility::screenshots::{
+    get_all_display, os_type, take_screenshot_all_displays, take_screenshot_area,
+    take_screenshot_display,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let screens = get_all_display();
+    for screen in &screens {
+        println!("screen {:?}", screen);
+    }
+    os_type();
+    take_screenshot_all_displays()
+        .unwrap()
+        .save("target/0.jpg")
+        .unwrap();
+    for screen in &screens {
+        take_screenshot_display(*screen)
+            .unwrap()
+            .save(format!("target/{}.jpg", screen.display_info.id))
+            .unwrap();
+    }
+    take_screenshot_area(screens[0], 0, 0, 800, 400)
+        .unwrap()
+        .save("target/1.png")
+        .unwrap();
 }
