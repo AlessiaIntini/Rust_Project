13ab63c0ba3fffbf1a9bfc817099ac55461cd38a
ALESSIA INTINI S309895 <s309895@studenti.polito.it>
2023-12-26 17:31:54 +0100

    fixed border

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -393,7 +393,7 @@ impl RustScreenRecorder {
                     println!("ctx: {:?}", ctx.available_rect());
                     self.screenshot = take_screenshot_area(
                         self.screens[self.screen_index.unwrap() as usize].clone(),
-                        (self.border_size.x/2.) as i32,
+                        (self.border_size.x-11.) as i32,
                         display_info.height as i32 - self.image_size.y as i32 - 7,
                         self.image_size.x as u32,
                         self.image_size.y as u32,
